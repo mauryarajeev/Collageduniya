3e4b75a088f7589cb2bab8ff06ce8d3b0f988bb4
Maurya Rajeev <104668455+mauryarajeev@users.noreply.github.com>
2023-10-08 20:14:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import './App.css'; // Make sure to import your CSS file
+import './App.css'; 
 import Header from './components/Header';
 import Navbar from './components/NavBar';
 import MainBody from './components/MainBody';
-// Import your components here
 const backgroundImageURL = 'https://officechai.com/wp-content/uploads/2016/12/IIM-A.jpg';
 
 function App() {
   const appStyles = {
     backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.2)), url(${backgroundImageURL})`,
-    backgroundSize: 'cover', // Cover the entire container
-    backgroundRepeat: 'no-repeat', // Don't repeat the image
-    backgroundAttachment: 'fixed', // Fixed background
+    backgroundSize: 'cover', 
+    backgroundRepeat: 'no-repeat', 
+    backgroundAttachment: 'fixed', 
     minHeight: '100vh', // Ensure the container takes up the full viewport height
     display: 'flex', // Make the content flex to cover entire screen
-    flexDirection: 'column', // Arrange the content in a column
+    flexDirection: 'column', 
   };
   return (
     <div className='AppContainer' style={appStyles}>
